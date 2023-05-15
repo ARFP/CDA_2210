76cbbe28fd8cce34f415bccc4c458d66ef402456
CRM202A <mdevoldere@users.noreply.github.com>
2023-05-15 12:54:49 +0200

    Correction api transactions

---
+++ Web/ASP/BankTransactions/BankTransactions.Database/Migrations/20230515080504_v2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankTransactions.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "transaction_amount",
+                table: "bank_transactions",
+                type: "decimal(7,2)",
+                precision: 7,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "transaction_amount",
+                table: "bank_transactions",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(7,2)",
+                oldPrecision: 7,
+                oldScale: 2);
+        }
+    }
+}
