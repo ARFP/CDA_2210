a7f18b1f5a3b2527915e94ee12d5f6d742268f02
CRM202A <mdevoldere@users.noreply.github.com>
2023-05-16 16:59:45 +0200

    Début Vues

---
+++ Web/ASP/BankTransactions/BankTransactions.WebApp/Controllers/ExampleController.cs
@@ -0,0 +1,41 @@
+﻿using BankTransactions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankTransactions.WebApp.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new object());
+        }
+
+        public IActionResult NamedView()
+        {
+            return View("MySuperView");
+        }
+
+        public IActionResult DataView()
+        {
+            ViewData["chaine1"] = "Voici une super chaine de caractères !";
+
+            ViewBag.chaine1 = "Voilà une autre chaîne, avec un chapeau !";
+
+            return View();
+        }
+
+        public IActionResult ModelView()
+        {
+            BankTransaction transaction = new()
+            {
+                Transaction_Id = 22,
+                Transaction_Date = DateTime.Now,
+                Transaction_From = 23121456987,
+                Transaction_To = 10236547898,
+                Transaction_Amount = (decimal) 1000.45
+            };
+
+            return View(transaction);
+        }
+    }
+}
--- Web/ASP/BankTransactions/BankTransactions.WebApp/Controllers/HomeController.cs
+++ Web/ASP/BankTransactions/BankTransactions.WebApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace BankTransactions.WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacy(int id)
         {
             return View();
         }
