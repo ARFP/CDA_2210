44ef629223cb469fb4661dcecee88ed0b5bd337a
CRM202A <mdevoldere@users.noreply.github.com>
2023-05-31 15:02:54 +0200

    ajout de commentaires

--- Web/exo-codes-postaux/js/ZipCode.js
+++ Web/exo-codes-postaux/js/ZipCode.js
@@ -1,11 +1,16 @@
 class ZipCode 
 {
+    // constructeur Javascript
     constructor()
     {
         this.datasource = "https://arfp.github.io/tp/web/frontend/zipcodes/zipcodes.json";
         this.zipcodes = [];
     }
 
+    /**
+     * Charge des données distantes et stocke le résultat dans l'attribut this.zipcodes
+     * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+     */
     async load() {
         try {
             let response = await fetch(this.datasource);
@@ -18,6 +23,11 @@ class ZipCode
     }
 
 
+    /** 
+     * Recherche des éléments dans la collection this.zipcodes
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+     * @param {int|string} zipcode le code postal ou nom de ville à rechercher
+     */
     search(zipcode)
     {
         let result = [];
@@ -29,8 +39,11 @@ class ZipCode
             }
         }*/
 
+        // la ligne suivante effectue le même traitement que le for ci-dessus
         result = this.zipcodes.filter(city => city.codePostal == zipcode);
+        
         console.log(result);
+
         this.fillUi(result);
 
 
@@ -40,14 +53,14 @@ class ZipCode
     {
         //let element = document.getElementById('result');
         let element = document.querySelector('#result');
-        element.innerHTML = "";
+        element.innerHTML = ""; // réinitialisation du contenu
 
         for(let city of result) {
             //element.innerHTML = "<li>"+ city.NomCommune+"</li>";
 
-            let li = document.createElement('li');
-            li.innerText = city.nomCommune;
-            element.appendChild(li);
+            let li = document.createElement('li'); // création d'un élément <li>
+            li.innerText = city.nomCommune + " " + city.codeCommune; // ajout de contenu dans l'élément <li> créé
+            element.appendChild(li); // ajout de l'élément <li> crée dans l'élément #result
         }
 
     }
