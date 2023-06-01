24f329f68703bda9729ebc59e210e35f641570f0
CRM202A <mdevoldere@users.noreply.github.com>
2023-06-01 15:08:35 +0200

    Demo flowers

--- Web/demo-flowers/js/main.js
+++ Web/demo-flowers/js/main.js
@@ -1,3 +1,27 @@
 const counter = document.getElementById('counter');
-const field = document.getElementById('field');
 const reset = document.getElementById('empty');
+const field = document.getElementById('field');
+
+let interval = undefined;
+
+const maFonction = () => {
+
+    if(counter.value > 0) {
+        let img = document.createElement('img');
+        img.src = "./img/flower1.svg";
+        field.appendChild(img);
+    } 
+    else if (counter.value == 0) {
+        clearInterval(interval);
+    }
+}
+
+counter.addEventListener('change', function() {
+
+    if(interval != undefined) {
+        clearInterval(interval);
+    }
+    interval = setInterval(maFonction, 1000/counter.value);
+    
+    
+});
