a6eced6bc15fd5e3770fe91f790b863fd5c31eb0
CRM202A <mdevoldere@users.noreply.github.com>
2023-07-04 12:46:32 +0200

    before comment + Add sort function

--- Web/exo-cereals/js/CerealsCollection.js
+++ Web/exo-cereals/js/CerealsCollection.js
@@ -2,16 +2,47 @@ import {Cereal} from "./Cereal.js";
 import { db } from "./db.js";
 class CerealsCollection{
     constructor(){
-        this.data=[]
+        this.initialData=[]
+        this.data=[];
     }
     async loadData(){
         let temp = await db.getDB("/cereals.json");
-        this.data = temp["data"];
+        this.initialData = temp["data"];
        // this.data = temp.data; // Equivalent de la ligne précedente
-        this.data = this.data.map(c=>new Cereal(c));
+        this.initialData = this.initialData.map(c=>new Cereal(c));
+        this.data = Array.from(this.initialData);
     }
     delRow(cerealId){
-        this.data = this.data.filter(d=>d.id != cerealId);
+        this.initialData = this.initialData.filter(d=>d.id != cerealId);
+        this.data = Array.from(this.initialData);
+    }
+    searchCereal(value){
+        this.data = this.initialData.filter(c=>c.name.toLowerCase().includes(value.toLowerCase()));
+    }
+    sortCategory(value){
+        switch(value){
+            case "1":
+                this.data = this.initialData.filter(c=>c.sodium<50);
+                break;
+            case "2":
+                this.data = this.initialData.filter(c=>c.sugars<1);
+                break;
+            case "3":
+                this.data = this.initialData.filter(c=>c.vitamins>=25 && c.fiber>=10);
+                break;
+            default:
+                this.data = Array.from(this.initialData);
+                break;
+        }
+    }
+    sortNutriscore(letters){
+        if(letters.length>0){
+            this.data = this.initialData.filter(c=>letters.includes(c.ns));
+        }
+        else{
+            this.data = Array.from(this.initialData);
+        }
     }
 }
+
 export {CerealsCollection}
--- Web/exo-cereals/js/main.js
+++ Web/exo-cereals/js/main.js
@@ -1,32 +1,45 @@
 import { CerealsCollection } from "./CerealsCollection.js";
 import { CerealsTable } from "./CerealsTable.js";
+import {CerealsEvent} from "./CerealsEvent.js";
 
 let cerealsCollection = new CerealsCollection();
+
+CerealsEvent.cerealsCollection = cerealsCollection;
+
 await cerealsCollection.loadData();
 
-let cerealsTableTbody = new CerealsTable(cerealsCollection);
-cerealsTableTbody.generateTable();
-let sortDirection = true;
-
-
-function generateClickEvent(e){
-    if(typeof(cerealsCollection.data[0][e.target.dataset.name])!="string"){
-        cerealsCollection.data.sort((x,y)=>x[e.target.dataset.name]-y[e.target.dataset.name]);
-    }
-    else{
-        cerealsCollection.data.sort((x,y)=>x[e.target.dataset.name].localeCompare(y[e.target.dataset.name]));
-    }
-    if(sortDirection){
-        cerealsCollection.data.reverse();
-    }
-    sortDirection=!sortDirection;
-    cerealsTableTbody.generateTable();
-}
+let cerealsTable = new CerealsTable(cerealsCollection);
+cerealsTable.generateTable();
 
 let head=document.querySelectorAll(".keys");
 
 for(let h of head){
     h.addEventListener("click",(e)=>{
-        generateClickEvent(e);
+        CerealsEvent.columnSortEvent(e);
+        cerealsTable.generateTable();
     })
 }
+document.getElementById("inputSearch").addEventListener("input",(e)=>{
+        CerealsEvent.searchCereal(e.target.value);
+        cerealsTable.generateTable();
+});
+document.getElementById("categoriesSelect").addEventListener("change",(e)=>{
+    CerealsEvent.sortCategory(e.target.value);
+    cerealsTable.generateTable();
+});
+let inputList = document.querySelectorAll('#cerealsNs input[type="checkbox"]');
+inputList.forEach(i=>{i.addEventListener("click",(e)=>{
+    let letters =[]; 
+    inputList.forEach(c=>{
+        if(c.checked){
+            letters.push(c.id);
+        }
+    });
+    console.log(letters)
+
+    CerealsEvent.sortNutriscore(letters);
+    cerealsTable.generateTable();
+
+
+    })
+});
